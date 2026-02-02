e6f3eea079bfb6bcafb8775c7aed1ca7e9c4d94f
santorbrands <234061587+santorbrands@users.noreply.github.com>
2026-02-02 20:38:32 +0700

    Change dish image format from PNG to WEBP

--- src/lib/dishImages.ts
+++ src/lib/dishImages.ts
@@ -1,5 +1,5 @@
 export const getDishImage = (dishId: string): string | undefined => {
   // Map dish ID to image path in public/images folder
   // All images are .png
-  return `/images/${dishId}.png`;
+  return `/images/${dishId}.webp`;
 };
