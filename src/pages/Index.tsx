654db863013d78ec3b57bc89d2f5b4dbdeaf7005
santor <234061587+santorbrands@users.noreply.github.com>
2026-03-15 18:12:40 +0700

    s

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -271,10 +271,10 @@ const Index = ({ initialBrand }: IndexProps) => {
       />
       
       {/* Scroll Progress Bar */}
-      <div className="fixed top-0 left-0 right-0 z-[100] h-0.5 bg-transparent">
+      <div className="fixed top-0 left-0 right-0 z-[100] h-1 bg-transparent">
         <div 
-          className="h-full bg-white transition-all duration-150 ease-out"
-          style={{ width: `${scrollProgress * 100}%` }}
+          className="h-full bg-white transition-all duration-75 ease-linear"
+          style={{ width: `${scrollProgress * 100}%`, willChange: 'width' }}
         />
       </div>
 
